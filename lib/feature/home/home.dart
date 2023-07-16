9d158bd8216afc4570fcbb5b618d25d2593b3d22
yurecKovalchuk <KovalchukYurec@hotmail.com>
2023-07-16 15:37:23 +0300

    refactor: move BottomNavigationBar to package widgt

---
+++ lib/feature/home/home.dart
@@ -0,0 +1,1 @@
+export 'package:weather/feature/home/screen/home_screen.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,26 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
-import 'feature/feature.dart';
-import 'models/models.dart';
+import 'package:weather/feature/feature.dart';
+import 'package:weather/models/class_city_location.dart';
 
 void main() => runApp(WeatherApp());
 
 class WeatherApp extends StatefulWidget {
-  const WeatherApp({
-    super.key,
-    City? selectedCity,
-  });
+  const WeatherApp({super.key});
 
   @override
   State<WeatherApp> createState() => _WeatherApp();
 }
 
 class _WeatherApp extends State<WeatherApp> {
-  City? selectedCity;
-  int _currentIndex = 1;
 
-  List<City> cities = [
-    City(name: "Kiev", location: Location(lat: 50.4501, lon: 30.5234)),
-    City(name: "Lviv", location: Location(lat: 49.8397, lon: 24.0297)),
-    City(name: "Odesa", location: Location(lat: 46.4825, lon: 30.7233)),
-  ];
-
-  Widget getPage(int index) {
-    if (index == 0) {
-      return LocationScreen(
-        onSelectedCity: onSelectCity,
-        cities: cities,
-      );
-    } else if (index == 1) {
-      return WeatherScreen(
-        selectedCity: selectedCity,
-      );
-    }
-    return MenuScreen(
-      selectedCity: selectedCity,
-    );
-  }
-
-  void onSelectCity(City city) {
-    _currentIndex = 1;
-    selectedCity = city;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    selectedCity = cities.first;
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: getPage(_currentIndex),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentIndex,
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.location_on_outlined),
-              label: "location",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.add),
-              label: "weather",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.list),
-              label: "menu",
-            ),
-          ],
-        ),
-      ),
+    return  const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
